e17bfd1d1bf4f7c6a1e5eab06a9e40bf4bad6598
Ezz Aldin <zezomomhamed74@gmail.com>
2026-05-11 17:24:54 +0300

    lab 4

--- src/components/Dashborad/Sidebar/Sidebar.jsx
+++ src/components/Dashborad/Sidebar/Sidebar.jsx
@@ -1,22 +1,32 @@
+import { NavLink } from "react-router-dom";
 import styles from "./Sidebar.module.css";
 function Sidebar() {
   const links = [
-    { icon: "fa-chart-line", title: "Status" },
-    { icon: "fa-users", title: "Users Management" },
-    { icon: "fa-building", title: "Projects Management" },
+    { icon: "fa-chart-line", title: "Status", path: "/" },
+    { icon: "fa-users", title: "Users Management", path: "/users" },
+    { icon: "fa-building", title: "Projects Management", path: "/projects" },
     { icon: "fa-building", title: "Developers Management" },
     { icon: "fa-globe", title: "CMS" },
     { icon: "fa-message", title: "Live Chat" },
   ];
+
   return (
     <aside className={`min-vh-100 py-4 ${styles.sidebar}`}>
-      <div className="title">Dashboard</div>
+      {/* Title */}
+      <div className="px-4 fw-semibold fs-4 mb-3">Dashboard</div>
+      {/* Nav Links */}
       <nav>
-        {links.map((item) => (
-          <div className="navItem">
-            <i className={`fa-solid ${item.icon}`}></i>
-            <span className="linkTitle">{item.title}</span>
-          </div>
+        {/* Link Item */}
+        {links.map((item, index) => (
+          // Link
+          <NavLink
+            to={item.path}
+            key={index}
+            className={`${styles.navItem} py-4 px-3 d-flex align-items-center gap-3`}
+          >
+            <i className={`fa-solid ${item.icon} ${styles.icon}`}></i>
+            <span className={styles.linkTitle}>{item.title}</span>
+          </NavLink>
         ))}
       </nav>
     </aside>
